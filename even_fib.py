acd492f75ef3447f45e0bd2b8ac8bb53bfcbca26
Clive Moses <clivemoses@gmail.com>
2024-06-24 13:20:04 +0100

    Updated even_fib.py to display some helpful messages while executing ...

--- even_fib.py
+++ even_fib.py
@@ -1,11 +1,11 @@
-t = int(input().strip())
+t = int(input('How many even sums do you want to generate? ').strip())
 for a0 in range(t):
-    n = int(input().strip())
+    n = int(input('Please enter series limit: ').strip())
     fib = [1,1]
     while fib[-1] <= n:
         fib.append(fib[-1] + fib[-2])
     if fib[-1] > n:
         del fib[-1]
     even_fib = [f for f in fib if f%2 == 0]
-    print(sum(even_fib))
-    
+    print('Sequence generated:', fib)
+    print('The sum of the even Fibonacci numbers <=', n, 'is', sum(even_fib))
